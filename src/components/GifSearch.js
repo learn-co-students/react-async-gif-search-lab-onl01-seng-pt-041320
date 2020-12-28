02691276790a1b8dc763fac6df7c74c30244f70a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 21:29:01 -0800

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 
 class GifSearch extends Component{
     state = {
-        query: ""
+        query: "",
+        submittedData: []
     }
+
+    handleQueryChange = event => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        let formData = {query: this.state.query};
+        let data_arr = this.state.submittedData.concat(formData);
+        this.setState({submittedData: data_arr});
+    }
+
     render(){
         return(
             <div>
                 <form align="right" >
                     <label>Enter a Search Term:</label><br></br>
                     <input type="text" name="search" /><br></br>
-                    <input type="submit" value={this.state.query} />
+                    <input type="submit" onChange={event => this.handleQueryChange(event)} value={this.state.query} />
                 </form>
             </div>
         )
