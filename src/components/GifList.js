1792b55fb8fecdc42bb30b2d1915ec98354f075e
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 21:15:21 -0800

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -5,7 +5,7 @@ class GifList extends Component{
         return(
             <div>
                 <ul>
-                    <li>{this.props.giphys.map(g => <img src={g.images.original.url} key={g.id}/>)}</li>
+                    <li>{this.props.giphys.map(g => <img src={g.images.original.url} key={g.id} alt={g.title}/>)}</li>
                 </ul>
             </div>
         )
