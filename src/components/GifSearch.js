d848c40d3f7f653b4bc3a43e3a3551a964644caa
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 21:22:42 -0800

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 class GifSearch extends Component{
+    state = {
+        query: ""
+    }
     render(){
         return(
             <div>
                 <form align="right" >
                     <label>Enter a Search Term:</label><br></br>
                     <input type="text" name="search" /><br></br>
-                    <input type="submit" value="submit" />
+                    <input type="submit" value={this.state.query} />
                 </form>
             </div>
         )
