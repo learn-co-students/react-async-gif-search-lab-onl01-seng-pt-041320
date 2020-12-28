9fa9767519870b29d336d62afbe9e2339a54a14a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 16:44:40 -0800

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class GifListContainer extends Component{
+    componentDidMount(){
+        fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=BRzpBD4aq2xAfR8ioC5XHlTZcYyp5Yc2')
+    }
     render(){
         return(
             <div></div>
