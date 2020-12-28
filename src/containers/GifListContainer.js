7bb3d5ed55a44abd139b1f3ee61bbe88e002eb29
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 16:47:45 -0800

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
 class GifListContainer extends Component{
+    state = {
+        giphys: []
+    }
+
     componentDidMount(){
         fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=BRzpBD4aq2xAfR8ioC5XHlTZcYyp5Yc2')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                giphys: data
+            })
+        })
     }
     render(){
         return(
