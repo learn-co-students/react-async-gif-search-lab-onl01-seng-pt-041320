9359a27f92e3bff2a2ba6a95046094a8cb9ed0d4
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-27 18:22:33 -0800

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -5,22 +5,24 @@ import GifList from '../components/GifList'
 
 class GifListContainer extends Component{
     state = {
-        giphys: []
+        GIF: []
     }
 
     componentDidMount(){
         fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=BRzpBD4aq2xAfR8ioC5XHlTZcYyp5Yc2&rating=g')
         .then(res => res.json())
-        .then(data => {
-            this.setState({
-                giphys: data
+        .then(giphys => {
+            console.log(giphys.data)
+            giphys.data.forEach((giphy) => this.setState({
+                GIF: giphy.embed_url
             })
+            )
         })
     }
     render(){
         return(
             <div>
-                {this.state.giphys}
+                {this.state.GIF}
                 <GifSearch />
                 <GifList Giphys={this.state.giphys}/>
             </div>
